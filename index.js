f987cf7a56b03b5e4d07c2253ab525944449379e
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-21 18:07:28 -0700

    commit tree 7baeb3a7db012c5be72f0dde6be32b96b82b0d39

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 2
+let num2 = 31
+
+let num3 = 20
+let num4 = 8
+
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random() * 10) 
+
+let mod = num3 % num4
+
+let max = Math.max(1, 15, 20, 12, 10, 5, 4, 19, 3)
